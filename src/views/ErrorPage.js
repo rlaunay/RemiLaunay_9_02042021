a7406dd3f3497eeaf0341ae1c2eb4026591ce13e
Remi Launay <rlaunay25@gmail.com>
2021-04-02 18:01:15 +0200

    add sorting on bills

--- src/views/ErrorPage.js
+++ src/views/ErrorPage.js
@@ -2,7 +2,7 @@ import VerticalLayout from './VerticalLayout.js'
 
 export default (error) => {
 
-  return (`
+    return (`
     <div class='layout'>
       ${VerticalLayout()}
       <div class='content'>
@@ -13,5 +13,5 @@ export default (error) => {
           ${error ? error : ""}
         </div>
     </div>`
-  )
+    )
 }
