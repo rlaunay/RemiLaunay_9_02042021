f184c7c1808d8ee79985e2b8e372f6381d91d3af
Remi Launay <rlaunay25@gmail.com>
2021-04-02 20:09:25 +0200

    block no img

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -12,41 +12,52 @@ export default class NewBill {
         file.addEventListener("change", this.handleChangeFile)
         this.fileUrl = null
         this.fileName = null
+        this.validFile = false
         new Logout({document, localStorage, onNavigate})
     }
 
     handleChangeFile = e => {
         const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-        const filePath = e.target.value.split(/\\/g)
-        const fileName = filePath[filePath.length - 1]
-        this.firestore
+
+        if (file && !/\.(jpeg|png|jpg)$/gi.test(file.name)) {
+            this.validFile = false
+        } else if (file) {
+            this.firestore
             .storage
-            .ref(`justificatifs/${fileName}`)
+            .ref(`justificatifs/${file.name}`)
             .put(file)
             .then(snapshot => snapshot.ref.getDownloadURL())
             .then(url => {
                 this.fileUrl = url
-                this.fileName = fileName
+                this.fileName = file.name
+                this.validFile = true
             })
+        }
     }
+
     handleSubmit = e => {
         e.preventDefault()
-        const email = JSON.parse(localStorage.getItem("user")).email
-        const bill = {
-            email,
-            type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-            name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-            amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-            date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
-            vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-            pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-            commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-            fileUrl: this.fileUrl,
-            fileName: this.fileName,
-            status: 'pending'
+        if (this.validFile) {
+            const email = JSON.parse(localStorage.getItem("user")).email
+            const bill = {
+                email,
+                type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+                name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+                amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+                date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+                vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+                pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+                commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+                fileUrl: this.fileUrl,
+                fileName: this.fileName,
+                status: 'pending'
+            }
+
+            this.createBill(bill)
+            this.onNavigate(ROUTES_PATH['Bills'])
+        } else {
+            alert('c\'est pas bon')
         }
-        this.createBill(bill)
-        this.onNavigate(ROUTES_PATH['Bills'])
     }
 
     // not need to cover this function by tests
